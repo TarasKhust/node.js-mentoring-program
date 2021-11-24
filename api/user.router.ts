be8df688f4194e8e82741f1d606ee1e07121c1c7
taras rushchak <taraskhust@gmail.com>
2021-11-24 18:41:26 +0200

    fix structure

--- api/user.router.ts
+++ api/user.router.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { UserController } from '../users/user.controller';
+import { UserController } from '../user/user.controller';
 import { schema, validator } from '../validation/user.validation';
 
 
--- config/typeorm.config.ts
+++ config/typeorm.config.ts
@@ -1,5 +1,5 @@
 import { ConnectionOptions } from 'typeorm';
-import { User } from '../users/user.entity';
+import { User } from '../user/user.entity';
 
 const config: ConnectionOptions = {
     type: 'postgres',
