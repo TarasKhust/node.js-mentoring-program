3aaead659625a627cf2826dfd616b831ae82493c
tarasrushchak <taraskhust@gmail.com>
2022-01-03 16:58:14 +0200

    fix deleted method

--- models/user.entity.ts
+++ models/user.entity.ts
@@ -22,7 +22,7 @@ export class UserEntity extends BaseEntity {
 
 
   @ManyToOne(() => GroupEntity, groups => groups.users, {
-       onUpdate: 'CASCADE', nullable: true
+       onUpdate: 'CASCADE', nullable: true,  onDelete: "SET NULL"
   })
       groups?: GroupEntity;
 
@@ -35,4 +35,14 @@ export class UserEntity extends BaseEntity {
 
   @UpdateDateColumn()
       updatedat!: Date;
+
+    foreignKeys: [
+        {
+            referencedTableName: 'users';
+            referencedColumnNames: ['id'];
+            columnNames: ['users'];
+            onDelete: 'CASCADE';
+
+        }
+    ] | undefined
 }
